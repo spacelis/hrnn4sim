375d6a905be4726686f840a042c517aa274e42ff
Wen Li <spacelis@gmail.com>
2018-06-11 16:09:52 +0100

    Add dropout rate to class parameters

--- hrnn4sim/seqsim_hrnn.py
+++ hrnn4sim/seqsim_hrnn.py
@@ -13,8 +13,7 @@ sentences. That is a paragraph of text can be seen as (sentence) sequences of
 from keras.layers.core import K
 from keras.models import Model
 from keras.layers import Dense, Embedding, Input, Lambda
-from keras.layers.wrappers import TimeDistributed
-from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers import TimeDistributed, LSTM, SimpleRNN
 
 from .vectorization import SubWordVectorizer
 from .base import ModelBase
@@ -22,9 +21,17 @@ from .base import ModelBase
 
 class SeqSimHRNN(ModelBase):
     """ Similiarity Model based on HierarchicalRNN """
-    def __init__(self, embedding_size=64, state_size=256, batch_size=100, **kwargs):  # pylint: disable=too-many-locals
+    def __init__(self, embedding_size=64, state_size=256, batch_size=1000,
+                 wenc_dor=0.2, senc_dor=0.2,
+                 **kwargs):  # pylint: disable=too-many-locals,too-many-arguments
+        """
+            :param wenc_dor: word encoding LSTM dropout rate
+            :param senc_dor: sentence encoding LSTM dropout rate
+        """
         super(SeqSimHRNN, self).__init__(**kwargs)
         self.embedding_size = embedding_size
+        self.wenc_dor = wenc_dor
+        self.senc_dor = senc_dor
         self.state_size = state_size
         self.batch_size = batch_size
 
@@ -48,17 +55,19 @@ class SeqSimHRNN(ModelBase):
 
 
         em = Embedding(len(self.vectorizer.alphabet) + 1, self.embedding_size, mask_zero=True)
-        encoder = SimpleRNN(self.state_size, dropout=0.2, recurrent_dropout=0.2)
+        encoder = SimpleRNN(self.state_size, dropout=self.wenc_dor, recurrent_dropout=self.wenc_dor)
         features = Dense(self.state_size)
+        self.other_layers.extend([em, encoder, features])
+
         seq_encoder = Lambda(lambda inputs: features(encoder(em(inputs))))
         encoderl = TimeDistributed(seq_encoder)
         codA = encoderl(A, mask=masking(A))
         codB = encoderl(B, mask=masking(B))
 
-        lstm1 = LSTM(self.state_size,
-                     dropout=0.2, recurrent_dropout=0.2, return_state=True)(codA)
-        lstm2 = LSTM(self.state_size,
-                     dropout=0.2, recurrent_dropout=0.2)(codB, initial_state=lstm1[1:])
+        lstm1 = LSTM(self.state_size, dropout=self.senc_dor, recurrent_dropout=self.senc_dor,
+                     return_state=True)(codA)
+        lstm2 = LSTM(self.state_size, dropout=self.senc_dor, recurrent_dropout=self.senc_dor)\
+            (codB, initial_state=lstm1[1:])
         output = Dense(1, activation='sigmoid')(lstm2)
         self.model = Model(inputs=[A, B], outputs=[output])
 
